b187a97b3cb7bbf4aa85386ab4d50fd51acffad8
DarkFriendz <trader.filus001@gmail.com>
2023-02-16 19:42:57 -0300

    Created HTML

--- venv/Structure/blueprints/en.py
+++ venv/Structure/blueprints/en.py
@@ -1,8 +1,13 @@
 #Assets
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect
 
 en = Blueprint('english', __name__, url_prefix='/en/')
 
 @en.route('/')
-def index():
-    return "en"
+@en.route('/<page>')
+def index(page=None):
+    if page != None:
+        if page == "work":
+            return render_template("en/work.html")
+    else:
+        return redirect('/en/work')
