e0d98b5da42f78c61c7635ab670d591e3b6c5e16
DarkFriendz <trader.filus001@gmail.com>
2023-02-17 22:46:40 -0300

    README

--- venv/Website/__init__.py
+++ venv/Website/__init__.py
@@ -1,15 +1,15 @@
 #Assets
-from flask import flask, redirect, flash
+from flask import Flask, redirect, flash
 
 #Class Web
 class web:
     #Config Web
     def __init__(self, config):
         #Created Website
-        self.website = flask(__name__)
+        self.website = Flask(__name__)
 
         #Secret Website
-        #self.website
+        self.website.secret_key = config['Secret']
 
     #Run Web
     def run(self, debug=False):
