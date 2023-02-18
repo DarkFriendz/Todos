2547cfb5bbaa70a73a55a53f3247a4d99aa5f5ec
DarkFriendz <trader.filus001@gmail.com>
2023-02-17 23:36:35 -0300

    En Home Page

--- venv/Website/blueprints/en/en.py
+++ venv/Website/blueprints/en/en.py
@@ -1,5 +1,15 @@
 #Assets
-from flask import Blueprint
+from flask import Blueprint, redirect
 
 #Blueprint
 en = Blueprint('english', __name__, url_prefix='/en/')
+
+#index
+@en.route('/')
+@en.route('/<page>')
+def index(page=None):
+    if page != None:
+        if page == 'home':
+            return "home"
+    else:
+        return redirect('/en/home')
