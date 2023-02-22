6bf52b54b6f81bbfee8317fffdffdc67cca9e879
DarkFriendz <trader.filus001@gmail.com>
2023-02-22 16:32:45 -0300

    DateFormat Home Page

--- venv/Website/blueprints/en/en.py
+++ venv/Website/blueprints/en/en.py
@@ -18,7 +18,15 @@ def index(page=None):
         if page == 'home':
             db.tasks()
             flashs = list(enumerate(get_flashed_messages(with_categories=True)))
-            return render_template('/en/home.html', flashs=flashs, tasks=db.info['content'])
+            tasks = db.info['content']
+            for id, task in enumerate(tasks):
+                if task[4] != None:
+                    tasklist = list(task)
+                    dateFormat = task[4].split('-')
+                    dateFormat = f'{dateFormat[2]}/{dateFormat[1]}/{dateFormat[0]}'
+                    tasklist[4] = dateFormat
+                    tasks[id] = tuple(tasklist)
+            return render_template('/en/home.html', flashs=flashs, tasks=tasks)
         elif page == 'addTask':
             flashs = list(enumerate(get_flashed_messages(with_categories=True)))
             return render_template('/en/addTask.html', flashs=flashs)
