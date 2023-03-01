2f786eeb9145ff54c17dd32479e2fc16b7f2b916
DarkFriendz <trader.filus001@gmail.com>
2023-03-01 18:36:44 -0300

    Start Project

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,5 @@
 #Assets
-from flask import Flask
+from flask import Flask, redirect, render_template, session
 
 #Class Website
 class web():
@@ -8,8 +8,39 @@ class web():
         #Create Website
         self.website = Flask(__name__)
 
+        #Secret Key Website
+        self.website.secret_key = config['Secret']
+
+        #Language Website
+        self.languages = ['en', 'pt', 'es']
+
     #Run Website
     def run(self, debug:bool=False):
+
+        @self.website.route('/')
+        @self.website.route('/<lang>')
+        def index(lang=None):
+            if lang != None:
+                for language in self.languages:
+                    if language == lang:
+                        session['language'] = lang
+                        return redirect(f'/{lang}/')
+
+            session['language'] = 'en'
+            return redirect('/en/')
+        
+        @self.website.route('/<lang>/')
+        @self.website.route('/<lang>/<page>')
+        def home(lang=None, page=None):
+            if lang != None:
+                if page != None:
+                    if page == 'home':
+                        return render_template('home.html')
+                else:
+                    return redirect(f'/{lang}/home')
+
+            session['language'] = 'en' 
+            return redirect(f'/en/')
         
         #Run
-        self.website.run()
+        self.website.run(debug=debug)
