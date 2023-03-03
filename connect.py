e15a16956cbb1718e762ef40af9635b7571c84d1
DarkFriendz <trader.filus001@gmail.com>
2023-03-03 18:17:31 -0300

    Home Page HTML Completed

--- connect.py
+++ connect.py
@@ -18,8 +18,8 @@ class db():
         #Table
         conn = sqlite3.connect(directory)
         cursor = conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS todo (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title TEXT NOT NULL, description TEXT, done VARCHAR(1) NOT NULL DEFAULT 'n', due_date DATE, created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP);''')
-        #cursor.execute('''INSERT INTO todo (title, description, due_date, created_at, updated_at) VALUES ('ola', 'ola', '2023-03-30', '2023-03-30 00:00:00', '2023-03-30 00:00:00')''')
+        cursor.execute('''CREATE TABLE IF NOT EXISTS todo (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title TEXT NOT NULL, description TEXT, done VARCHAR(1) NOT NULL DEFAULT 'N', due_date DATE, created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP);''')
+        #cursor.execute('''INSERT INTO todo (title, description, due_date, created_at, updated_at) VALUES ('ola', 'ola', '2023-02-30', '2023-02-30 00:00:00', '2023-02-30 00:00:00')''')
         conn.commit()
         conn.close()
 
@@ -31,19 +31,35 @@ class db():
                 cur.execute('''SELECT * FROM todo''')
                 data = []
                 for row in cur.fetchall():
-                    rows = {
-                        'id': row[0],
-                        'title': row[1],
-                        'description': row[2],
-                        'done': row[3],
-                        'due_date': row[4],
-                        'created_at': row[5],
-                        'updated_at': row[6]
-                    }
+                    now = datetime.now()
+                    now = datetime.strftime(now, '%Y%m%d%H%M%S')
+                    dataFormat = row[4].replace('-', '')+'000000'
+                    if now > dataFormat:
+                        rows = {
+                            'id': row[0],
+                            'title': row[1],
+                            'description': row[2],
+                            'done': row[3],
+                            'due_date': row[4],
+                            'created_at': row[5],
+                            'updated_at': row[6],
+                            'expired': True
+                        }
+                    else:
+                        rows = {
+                            'id': row[0],
+                            'title': row[1],
+                            'description': row[2],
+                            'done': row[3],
+                            'due_date': row[4],
+                            'created_at': row[5],
+                            'updated_at': row[6],
+                            'expired': False
+                        }
                     data.append(rows)
                 conn.close()
                 self.result['info'] = data
                 return self.result
         except:
-            if lang == 'en':
+            if self.result['info'] == None:
                 print('error')
