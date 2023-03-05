4af2229311b8e9eb07f69e8eae56565bdc57bd20
DarkFriendz <trader.filus001@gmail.com>
2023-03-05 16:36:47 -0300

    Stop

--- connect.py
+++ connect.py
@@ -32,7 +32,7 @@ class db():
                 cur.execute('''SELECT * FROM todo''')
                 data = []
                 for row in cur.fetchall():
-                    if row[4] != None:
+                    if row[4] != None and row[4] != '':
                         now = datetime.now()
                         now = datetime.strftime(now, '%Y%m%d%H%M%S')
                         dataFormat = row[4].replace('-', '')+'000000'
@@ -76,8 +76,30 @@ class db():
             if self.result['info'] == None:
                 print('error')
 
+    #Get Row
+    def getRow(self, id:str):
+        self.result = {'info': None, 'error': False}
+        try:
+            with sqlite3.connect(self.directory) as conn:
+                cur = conn.cursor()
+                cur.execute('''SELECT * FROM todo WHERE id=?''', id)
+                row = cur.fetchone()
+                row = {
+                        'id': row[0],
+                        'title': row[1],
+                        'description': row[2],
+                        'done': row[3],
+                        'due_date': row[4],
+                        'created_at': row[5],
+                        'updated_at': row[6]
+                }
+            self.result['info'] = row
+        except:
+            if self.result['info'] == None:
+                print('error')
+
     #Add Task
-    def addTask(self, request):
+    def addTask(self, request:list):
         self.result = {'info': None, 'error': False}
         try:
             with sqlite3.connect(self.directory) as conn:
@@ -101,8 +123,66 @@ class db():
                         cur.execute('''INSERT INTO todo (title, description, due_date, created_at, updated_at) VALUES (?, ?, ?, ?, ?)''', (request['title'], request['description'], date, date2, date2))
                     else:
                         cur.execute('''INSERT INTO todo (title, description, due_date, created_at, updated_at) VALUES (?, ?, ?, ?, ?)''', (request['title'], None, date, date2, date2))
-
                 conn.commit()
+            self.result['info'] = "Saved"
+        except:
+            if self.result['info'] == None:
+                print('error')
+
+    #Delete Task
+    def deletTask(self, id:str):
+        self.result = {'info': None, 'error': False}
+        try:
+            with sqlite3.connect(self.directory) as conn:
+                cur = conn.cursor()
+                cur.execute('''DELETE FROM todo WHERE id=?''', id)
+                conn.commit()
+            self.result['info'] == 'Deleted'
+        except:
+            if self.result['info'] == None:
+                print('error')
+
+    #Edit Task
+    def editTask(self, id:str, request:list):
+        self.result = {'info': None, 'error': False}
+        try:
+            with sqlite3.connect(self.directory) as conn:
+                cur = conn.cursor()
+                date = datetime.now()
+                date = datetime.strftime(date, '%Y-%m-%d %H:%M:00')
+                try:
+                    if request['done']:
+                        cur.execute('''UPDATE todo SET title=?, description=IFNULL(?, 'none'), done=?, due_date=IFNULL(?, 'none'), updated_at=? WHERE id=?''', (request['title'], request['description'], 'S', request['date'].split("T")[0], date, id))
+                except:
+                    cur.execute('''UPDATE todo SET title=?, description=IFNULL(?, 'none'), done=?, due_date=IFNULL(?, 'none'), updated_at=? WHERE id=?''', (request['title'], request['description'], 'N', request['date'].split("T")[0], date, id))
+                conn.commit()
+            self.result['info'] == 'Edited'
+        except:
+            if self.result['info'] == None:
+                print('error')
+
+    #Done Task
+    def Done(self, id:str):
+        self.result = {'info': None, 'error': False}
+        try:
+            with sqlite3.connect(self.directory) as conn:
+                cur = conn.cursor()
+                cur.execute('''UPDATE todo SET done=? WHERE id=?''', ('S', id))
+                conn.commit()
+            self.result['info'] == 'Done'
+        except:
+            if self.result['info'] == None:
+                print('error')
+
+    #NotDone Task
+    def NotDone(self, id:str):
+        self.result = {'info': None, 'error': False}
+        try:
+            with sqlite3.connect(self.directory) as conn:
+                cur = conn.cursor()
+                cur.execute('''UPDATE todo SET done=? WHERE id=?''', ('N', id))
+                conn.commit()
+            self.result['info'] == 'NotDone'
         except:
             if self.result['info'] == None:
                 print('error')
--- website/__init__.py
+++ website/__init__.py
@@ -37,16 +37,21 @@ class web():
         #Home
         @self.website.route('/<lang>/')
         @self.website.route('/<lang>/<page>')
-        def home(lang=None, page=None):
+        @self.website.route('/<lang>/<page>/<id>')
+        def home(lang=None, page=None, id=None):
             if lang != None:
                 if page != None:
                     if page == 'home':
                         self.db.getAll(session['language'])
                         session['language'] = lang
                         return render_template('home.html', rows=self.db.result['info'])
-                    if page == 'addTask':
+                    elif page == 'addTask':
                         session['language'] = lang
                         return render_template('task.html')
+                    elif page == 'edit':
+                        self.db.getRow(id)
+                        session['language'] = lang
+                        return render_template('edit.html', row=self.db.result['info'])
                 else:
                     session['language'] = lang
                     return redirect(f'/{lang}/home')
@@ -55,13 +60,14 @@ class web():
             return redirect(f'/en/')
         
         #Execute
+        @self.website.route('/<lang>/execute/<func>/<id>', methods=['POST', 'GET'])
         @self.website.route('/<lang>/execute/<func>', methods=['POST'])
-        def execute(lang, func):
+        def execute(lang=None, func=None, id=None):
+            #Add Task
             if func == 'addTask':
                 if request.form['title'] != '' and len(request.form['title']) >= 3:
                     self.db.addTask(request.form)
-                    #return redirect('/en/addTask')
-                    return request.form
+                    return redirect(f'/{lang}/addTask')
                 else:
                     if lang == 'en':
                         flash('Title is too short!')
@@ -71,8 +77,29 @@ class web():
                         flash('¡El título es demasiado corto!')
                     else:
                         flash('Title is too short!')
-                    
-                    return redirect('/en/addTask')
+
+                    session['language'] = lang
+                    return redirect(f'/{lang}/addTask')
+
+            #Delete Task
+            elif func == 'delet':
+                self.db.deletTask(id)
+                session['language'] = lang
+                return redirect(f'/{lang}/home')
+            
+            #Edit Task
+            elif func == 'edit':
+                self.db.editTask(id, request.form)
+                return redirect(f'/{lang}/edit/{id}')
+            
+            #Done Task
+            elif func == 'edit':
+                self.db.Done(id)
+                return redirect(f'/{lang}/home')
+
+            elif func == None:
+                session['language'] = lang
+                return redirect(f'/{lang}/home')
 
         #Run
         self.website.run(debug=debug)
