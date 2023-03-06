177b85db62afc03f25e2ed0bfc11f90052c06719
DarkFriendz <trader.filus001@gmail.com>
2023-03-06 20:59:27 -0300

    Search Completed

--- website/static/js/template.js
+++ website/static/js/template.js
@@ -5,6 +5,12 @@ const select = document.getElementById("select");
 
 // Select 
 select.addEventListener("change", function() {
+    const rows = document.getElementById("Table").rows;
+    for (let i = 0; i < rows.length; i++) {
+        if(i >= 1) {
+            rows[i].style.display = "table-row";
+        }
+    }
     switch(select.value) {
         case 'title':
             search.style.display = "block";
@@ -21,14 +27,35 @@ select.addEventListener("change", function() {
         case 'done':
             search.style.display = "none";
             searchDate.style.display = "none";
+            for (let i = 0; i < rows.length; i++) {
+                if(i >= 1) {
+                    if(rows[i].className != "Done") {
+                        rows[i].style.display = "none";
+                    }
+                }
+            }
         break;
         case 'notdone':
             search.style.display = "none";
             searchDate.style.display = "none";
+            for (let i = 0; i < rows.length; i++) {
+                if(i >= 1) {
+                    if(rows[i].className == "Done") {
+                        rows[i].style.display = "none";
+                    }
+                }
+            }
         break;
         case 'expired':
             search.style.display = "none";
             searchDate.style.display = "none";
+            for (let i = 0; i < rows.length; i++) {
+                if(i >= 1) {
+                    if(rows[i].className != "Expired") {
+                        rows[i].style.display = "none";
+                    }
+                }
+            }
         break;
     }
 });
@@ -38,10 +65,41 @@ select.addEventListener("change", function() {
 search.addEventListener("input", function() {
     const value = search.value.toLowerCase();
     const rows = document.getElementById("Table").rows;
+    
+    if(select.value == 'title') {
+        for (let i = 0; i < rows.length; i++) {
+            if(i >= 1) {
+                if (rows[i].cells[1].textContent.toLowerCase().indexOf(value) !== -1) {
+                    rows[i].style.display = "table-row";
+                } else {
+                    rows[i].style.display = "none";
+                }
+            }
+        }
+    } else if(select.value == 'description') {
+        for (let i = 0; i < rows.length; i++) {
+            if(i >= 1) {
+                if (rows[i].cells[2].textContent.toLowerCase().indexOf(value) !== -1) {
+                    rows[i].style.display = "table-row";
+                } else {
+                    rows[i].style.display = "none";
+                }
+            }
+        }
+    }
+});
 
+// Search Date
+
+searchDate.addEventListener("change", function() {
+    const rows = document.getElementById("Table").rows;
     for (let i = 0; i < rows.length; i++) {
         if(i >= 1) {
-            console.log(rows[i]);
+            if (rows[i].cells[3].textContent == searchDate.value.split("T")[0]) {
+                rows[i].style.display = "table-row";
+            } else {
+                rows[i].style.display = "none";
+            }
         }
     }
 });
